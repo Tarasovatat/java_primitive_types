60173386c43d968e1a147faa95421ec1b7917ba1
Татьяна Тарасова <kryachkovat@gmail.com>
2022-07-16 19:51:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+        public static void main(String[] args) {
+
+            int ticketPrice = 15_000;
+            int oneBonusMilePrice = 20;
+
+            int bonusMilesAmount = ticketPrice / oneBonusMilePrice;
+
+            System.out.println(bonusMilesAmount);
+            
+    }
+}
